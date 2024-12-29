ddbf3d03b5eccebaebf4880d2366a8571d6a6ab6
Ann109 <andersonhloa@gmail.com>
2024-12-29 15:45:16 +0300

    Update homework4update.py

--- homework4update.py
+++ homework4update.py
@@ -14,10 +14,13 @@ dp = Dispatcher(storage=storage)
 dp.include_router(router)
 
 class UserState(StatesGroup):
-    age = State()  # состояния возраста
-    growth = State()  # ...состояния роста
-    weight = State()  # ...состояния веса
+    age = State()  # возраст
+    growth = State()  # рост
+    weight = State()  # вес
 
+
+def is_valid_number(value):
+    return value.isdigit() and int(value) > 0
 
 # Ответ на текстовое сообщение 'Calories'
 @router.message(F.text.casefold().contains('calories'))  # F.text - ссылка на текст сообщения, используется
@@ -29,22 +32,29 @@ async def set_age(message: types.Message, state: FSMContext):
 # Хендлер для обработки возраста
 @router.message(UserState.age)
 async def set_growth(message: types.Message, state: FSMContext):
-        await state.update_data(age=int(message.text)) # обновляет данные состояния, сохраняя возраст пользователя
-        await message.answer("Введите свой рост (в см):")
-        await state.set_state(UserState.growth) # устанавливаем состояние growth, где бот ожидает ввода роста
+        if is_valid_number(message.text):
+            await state.update_data(age=int(message.text)) # обновляет данные состояния, сохраняя возраст пользователя
+            await message.answer("Введите свой рост (в см):")
+            await state.set_state(UserState.growth) # устанавливаем состояние growth, где бот ожидает ввода роста
+        else:
+            await message.answer("Возраст должен быть положительным числом. Пожалуйста, введите корректное значение.")
 
 
 # Хендлер для обработки роста
 @router.message(UserState.growth)
 async def set_weight(message: types.Message, state: FSMContext):
+    if is_valid_number(message.text):
         await state.update_data(growth=int(message.text)) # обновляет данные состояния, сохраняя рост пользователя
         await message.answer("Введите свой вес (в кг):")
         await state.set_state(UserState.weight) # устанавливаем состояние weight, где бот ожидает ввода вес
+    else:
+        await message.answer("Рост должен быть положительным числом. Пожалуйста, введите корректное значение.")
 
 
 # Хендлер для обработки веса и вычисления нормы калорий
 @router.message(UserState.weight)
 async def send_calories(message: types.Message, state: FSMContext):
+    if is_valid_number(message.text):
         await state.update_data(weight=int(message.text)) # обновляет данные состояния, сохраняя вес пользователя
         data = await state.get_data() # извлекаем все данные введенные пользователем (возраст, рост, вес)
 
@@ -54,17 +64,16 @@ async def send_calories(message: types.Message, state: FSMContext):
 
         # Формула Миффлина - Сан Жеора для мужчин
         calories = 10 * weight + 6.25 * growth - 5 * age + 5
-
         await message.answer(f"Ваша норма калорий: {calories:.2f} ккал в день.")
-
         await state.clear()  # Завершение машины состояний
- 
+    else:
+        await message.answer("Вес должен быть положительным числом. Пожалуйста, введите корректное значение.")
 
 # Команда start
 @dp.message(Command("start"))
 async def start_form(message: Message):
-    await message.answer("Привет! Я бот, помогающий твоему здоровью., "
-                         "Напиши слово 'Calories'.")
+    await message.answer("Привет! Я бот, помогающий твоему здоровью. Если хочешь узнать свою суточную норму калорий, "
+                         "то напиши слово 'Calories'.")
 
 # Хендлер для перенаправления всех остальных сообщений на start
 @router.message(~F.text.casefold().contains('calories') and ~F.state(UserState.age) and ~F.state(UserState.growth)
